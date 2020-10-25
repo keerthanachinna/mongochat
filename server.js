e948ecc16c53cd009ce34b2dc7f7f487a6a150ca
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2020-10-25 23:44:07 +0530

    git hug choose

---
+++ server.js
@@ -0,0 +1,61 @@
+const mongo = require('mongodb');
+const client = require('socket.io').listen(4000).sockets;
+
+
+// connect mongo db
+mongo.connect('mongodb://127.0.0.1/mongochat',function(err, db) {
+    if (err) {
+        throw err
+
+    }
+    console.log("Mongodb connected");
+
+    client.on('connection', function () {
+        let chat = db.collection('chats');
+        // create function to send staus
+        sendStaus = function (s) {
+            socket.emit('status', s);
+        }
+        // get chats from mongo collection
+        chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+            if (err) throw err;
+            // emit the mesage
+            socket.emit('output', res);
+        });
+        // handle input event
+        socket.on('input', function (data) {
+            let name = data.name;
+            let message = data.message;
+
+            // check for the name and message
+
+            if (name == '' || message == '') {
+                // send error status
+                sensStatus('Please enter the name and message');
+
+
+            }
+            else {
+                // insert message
+                chat.insert({ name: name, message: message }, function () {
+                    client.emit('output', [data]);
+                    // send status object
+                    sendStaus({
+                        message: "message sent",
+                        clear: true
+                    })
+                })
+            }
+        });
+        // handle clear 
+        socket.on('clear', function (data) {
+            // remove all chats from collection
+            chat.remove({}, function () {
+                // emit cleared
+                socket.emt();
+            })
+        })
+
+    })
+});
+
